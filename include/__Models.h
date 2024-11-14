6231be59a427f1ef761b90fa6b83577852e6cbc0
Membrane <pradoguillermojr@gmail.com>
2024-11-13 19:33:53 -0800

    added things to get sounds working

---
+++ include/__Models.h
@@ -0,0 +1,16 @@
+#ifndef __MODELS_H
+#define __MODELS_H
+
+
+class __Models
+{
+    public:
+        __Models();
+        virtual ~__Models();
+
+    protected:
+
+    private:
+};
+
+#endif // __MODELS_H
---
+++ src/__Models.cpp
@@ -0,0 +1,11 @@
+#include "__Models.h"
+
+__Models::__Models()
+{
+    //ctor
+}
+
+__Models::~__Models()
+{
+    //dtor
+}
